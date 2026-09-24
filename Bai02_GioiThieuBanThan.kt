c645cd5c4a64eaad70fbc22310a6694d70ca989c
Candy6654 <122604860+Candy6654@users.noreply.github.com>
2026-09-24 14:44:51 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,8 @@
+// Nguyễn Thanh Phước - 25810036
+fun main() {
+    val hoTen: String = "Nguyễn Thanh Phước"
+    val namSinh: Int = 2004
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+    println("Xin chào! Tôi tên là $hoTen, sinh năm $namSinh và năm nay tôi $tuoi tuổi.")
+}
